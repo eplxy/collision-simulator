ab050189e3159ceb7bace243ab5be23ec7913bb5
sabbb-a <103955672+sabbb-a@users.noreply.github.com>
2022-10-24 18:56:33 -0400

    Add files via upload
    display the parameters menu by clicking on a circle

--- CollisionSimulator/app/src/main/java/edu/vanier/collisionsimulator/ui/CollisionMenuController.java
+++ CollisionSimulator/app/src/main/java/edu/vanier/collisionsimulator/ui/CollisionMenuController.java
@@ -4,22 +4,18 @@
  */
 package edu.vanier.collisionsimulator.ui;
 
+import edu.vanier.collisionsimulator.simulator.CircleObj;
+import java.io.IOException;
 import java.util.ArrayList;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Bounds;
-import javafx.scene.Group;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Shape;
-import javafx.util.Duration;
 
 /**
  *
@@ -27,24 +23,72 @@ import javafx.util.Duration;
  */
 public class CollisionMenuController {
     @FXML
-    Pane animationPane;
+    private Pane animationPane;
     @FXML
-    Button saveBtn;
+    private AnchorPane parametersPane;
     @FXML
-    Button wtvBtn;
-     
+    private VBox parametersVBox; 
+    @FXML
+    private ChoiceBox objectChoice;
     
-    public void initialize(ArrayList<Shape> nodesList){
-        animationPane.getChildren().addAll(nodesList);
-        animationPane.setOnMouseClicked((event)->{
-            System.out.println("dragged mouse over pane!");});
-        saveBtn.setOnAction((event)->{
-            System.out.println("it works!");}
-        );
-        wtvBtn.setOnAction((event)->{
-            System.out.println("it works!");}
-        );
-        
-       
+    private static ObservableList<String> objectChoices = FXCollections.observableArrayList();
+
+    public static ObservableList<String> getObjectChoices(){
+        return objectChoices;
     }
+
+    public static void setObjectChoices(ObservableList<String> newObjectChoices){
+       objectChoices = newObjectChoices;
+    }
+    
+    public void initialize(ArrayList<Shape> nodesList) throws IOException{
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/objectParameters.fxml"));
+//        loader.setController(new ParametersController());
+//        parametersVBox.getChildren().add(loader.load());
+        
+        animationPane.getChildren().addAll(nodesList);
+        
+        
+    }   
+    
+    public void addShape(Shape shape) {
+        animationPane.getChildren().add(shape);
+    }
+    
+
+    public Pane getAnimationPane() {
+        return animationPane;
+    }
+
+    public void setAnimationPane(Pane animationPane) {
+        this.animationPane = animationPane;
+    }
+
+    public AnchorPane getParametersPane() {
+        return parametersPane;
+    }
+
+    public void setParametersPane(AnchorPane parametersPane) {
+        this.parametersPane = parametersPane;
+    }
+
+    public VBox getParametersVBox() {
+        return parametersVBox;
+    }
+
+    public void setParametersVBox(VBox parametersVBox) {
+        this.parametersVBox = parametersVBox;
+    }
+
+    public ChoiceBox getObjectChoice() {
+        return objectChoice;
+    }
+
+    public void setObjectChoice(ChoiceBox objectChoice) {
+        this.objectChoice = objectChoice;
+    }
+
+    
+    
+    
 }
---
+++ CollisionSimulator/app/src/main/java/edu/vanier/collisionsimulator/ui/ParametersController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.collisionsimulator.ui;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Shape;
+
+/**
+ *
+ * @author sabri
+ */
+public class ParametersController {
+    public  void initialize() {
+       
+    }      
+}
