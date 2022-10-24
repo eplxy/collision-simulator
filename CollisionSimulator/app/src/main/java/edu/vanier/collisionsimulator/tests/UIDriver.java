f1a8afd656f21874109be7a6a3627308f4854c69
sabbb-a <103955672+sabbb-a@users.noreply.github.com>
2022-10-24 18:50:41 -0400

    Add files via upload

--- CollisionSimulator/app/src/main/java/edu/vanier/collisionsimulator/tests/UIDriver.java
+++ CollisionSimulator/app/src/main/java/edu/vanier/collisionsimulator/tests/UIDriver.java
@@ -8,6 +8,7 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -17,13 +18,19 @@ import javafx.stage.Stage;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
+import edu.vanier.collisionsimulator.simulator.CircleObj;
+import edu.vanier.collisionsimulator.ui.ParametersController;
+import javafx.scene.layout.BorderPane;
 
 public class UIDriver extends Application {
 
-    private ArrayList<Shape> nodes;
+    //private ArrayList<Shape> nodes;
 
     public static void main(String[] args) {
         launch(args);
@@ -32,116 +39,24 @@ public class UIDriver extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Drag circles around to see collisions");
-        //Group animation = new Group();
 
-        nodes = new ArrayList<>();
-        nodes.add(new Circle(500, 400, 50));
-        nodes.add(new Circle(250, 600, 50));
-        nodes.add(new Circle(40, 200, 50));
-        for (Shape block : nodes) {
-            setDragListeners(block);
-        }
-        //animation.getChildren().addAll(nodes);
-        checkShapeIntersection(nodes.get(nodes.size() - 1));
-
+        
         try {
 
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CollisionMenu.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/CollisionMenu3.fxml"));
             edu.vanier.collisionsimulator.ui.CollisionMenuController menuController = new edu.vanier.collisionsimulator.ui.CollisionMenuController();
             loader.setController(menuController);
-            AnchorPane root = loader.load();
-
-            menuController.initialize(nodes);
-            Circle circle = new Circle(10, 10, 10);
-            circle.setLayoutX(20);
-            circle.setLayoutY(20);
-            nodes.add(circle);
-            root.getChildren().add(circle);
-
+            BorderPane root = loader.load();
+            CircleObj circle1 = new CircleObj(15, 20, 35, 50, 54, menuController);
             
-            Timeline timeline = new Timeline(new KeyFrame(Duration.millis(5), new EventHandler<ActionEvent>() {
-
-                double deltaX = 2;
-                double deltaY = 2;
-                Bounds bounds = root.getBoundsInLocal();
-                
-                
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    circle.setLayoutX(circle.getLayoutX() + deltaX);
-                    circle.setLayoutY(circle.getLayoutY() + deltaY);
-
-                    boolean rightBorder = circle.getLayoutX() >= (bounds.getMaxX() - circle.getRadius());
-                    boolean leftBorder = circle.getLayoutX() <= (bounds.getMinX() + circle.getRadius());
-                    boolean bottomBorder = circle.getLayoutY() <= (bounds.getMinY() - circle.getRadius());
-                    boolean topBorder = circle.getLayoutY() >= (bounds.getMaxY() + circle.getRadius());
-
-                    /*debug
-                    System.out.println("boundsXmin=" + bounds.getMinX() + " ,boundsXmax=" + bounds.getMaxX() + " ,boundsYmin=" + bounds.getMinY() + " ,boundsYmax=" + bounds.getMaxY());
-                    System.out.println("cX=" + circle.getLayoutX() + " ,cY=" + circle.getLayoutY() + " ,radius=" + circle.getRadius());
-                    System.out.println("top=" + topBorder + " ,bottom=" + bottomBorder + " ,left=" + leftBorder + " ,right=" + rightBorder);
-                    */
-                    if (rightBorder || leftBorder) {
-                        deltaX *= -1;
-                    }
-                    if (bottomBorder || topBorder) {
-                        deltaY *= -1;
-                    }
-                }
-            }));
-            timeline.setCycleCount(Animation.INDEFINITE);
-            timeline.play();
-
+            
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.sizeToScene();
-            primaryStage.show();
+            primaryStage.show(); 
         } catch (Exception e) {
             System.out.println(e);
         }
     }
-
-    public void setDragListeners(final Shape block) {
-        final Delta dragDelta = new Delta();
-
-        block.setOnMousePressed((MouseEvent mouseEvent) -> {
-            // record a delta distance for the drag and drop operation.
-            dragDelta.x = block.getLayoutX() - mouseEvent.getSceneX();
-            dragDelta.y = block.getLayoutY() - mouseEvent.getSceneY();
-            block.setCursor(Cursor.NONE);
-        });
-        block.setOnMouseReleased((MouseEvent mouseEvent) -> {
-            block.setCursor(Cursor.HAND);
-        });
-        block.setOnMouseDragged((MouseEvent mouseEvent) -> {
-            block.setLayoutX(mouseEvent.getSceneX() + dragDelta.x);
-            block.setLayoutY(mouseEvent.getSceneY() + dragDelta.y);
-            checkShapeIntersection(block);
-        });
-    }
-
-    private void checkShapeIntersection(Shape block) {
-        boolean collisionDetected = false;
-        for (Shape static_bloc : nodes) {
-            if (static_bloc != block) {
-                static_bloc.setFill(Color.GREEN);
-
-                Shape intersect = Shape.intersect(block, static_bloc);
-                if (intersect.getBoundsInLocal().getWidth() != -1) {
-                    collisionDetected = true;
-                }
-            }
-        }
-
-        if (collisionDetected) {
-            block.setFill(Color.BLUE);
-        } else {
-            block.setFill(Color.GREEN);
-        }
-    }
-
-    class Delta {
-
-        double x, y;
-    }
+    
 }
