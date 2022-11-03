671ea0b8ee841629c9f69f187cbd0e93415b2bb9
eplxy <>
2022-11-03 11:43:07 -0400

    steven's physics controller

--- CollisionSimulator/app/src/main/java/edu/vanier/collisionsimulator/controllers/PhysicsController.java
+++ CollisionSimulator/app/src/main/java/edu/vanier/collisionsimulator/controllers/PhysicsController.java
@@ -16,14 +16,18 @@ public class PhysicsController {
         double bVelY = objB.getVelocityY();
         double aMass = objA.getMass();
         double bMass = objB.getMass();
-
-        //velocity calculations with mass! STILL MISSING ANGLES
-        //TODO: implement angle interaction
+        
+        double aCenterX = objA.getPosX();
+        double aCenterY = objA.getPosY();
+        double bCenterX = objB.getPosX();
+        double bCenterY = objB.getPosY();
+        
+        CustomVector v = new CustomVector(aCenterX-bCenterX, aCenterY-bCenterY);
         CustomVector cvAVel = new CustomVector(((aMass - bMass) * aVelX + 2 * bMass * bVelX) / (aMass + bMass), ((aMass - bMass) * aVelY + 2 * bMass * bVelY) / (aMass + bMass));
         CustomVector cvBVel = new CustomVector(((bMass - aMass) * bVelX + 2 * aMass * aVelX) / (aMass + bMass), ((bMass - aMass) * bVelY + 2 * aMass * aVelY) / (aMass + bMass));
 
-        //CustomVector cvAVelM = new CustomVector(objB.getVelocityX(), objB.getVelocityY());
-        //CustomVector cvBVelM = new CustomVector(objA.getVelocityX(), objA.getVelocityY());
+        
+        
         
         return new CustomVector[]{cvAVel, cvBVel};
 
